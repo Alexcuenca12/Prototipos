ec4f90db4c79ef2b88d7f7652ae7fd52fe3faa23
Alexcuenca12 <mateosiavig@gmail.com>
2022-06-29 21:30:17 -0500

    Update-Servicios

---
+++ app/src/main/java/com/mateo/siavi/prototipos/numpadContra.java
@@ -0,0 +1,14 @@
+package com.mateo.siavi.prototipos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class numpadContra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numpad_contra);
+    }
+}
